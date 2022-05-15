915abb4e099c84d0cb6a0e1c9a7a9dc6c9b70fd3
Gustavo Bispo <38411510+gustavolbs@users.noreply.github.com>
2022-05-15 10:11:26 -0300

    add my first commit

---
+++ node/index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const mysql = require("mysql2");
+const { faker } = require("@faker-js/faker");
+const app = express();
+const port = 3000;
+
+const config = {
+  host: "db",
+  user: "root",
+  password: "root",
+  database: "nodedb",
+};
+
+const connection = mysql.createConnection(config);
+
+function createTable() {
+  connection.query(`
+    CREATE TABLE IF NOT EXISTS PEOPLE(
+        ID INT NOT NULL AUTO_INCREMENT, 
+        NAME VARCHAR(255) NOT NULL,
+        PRIMARY KEY (ID)
+    )`);
+}
+
+function response(results) {
+  return `
+  <h1>Full Cycle Rocks!</h1>
+  <ol>
+      ${results.map(({ name }) => `<li>${name}</li>`)}
+  </ol>
+  `;
+}
+
+connection.connect(() => {
+  createTable();
+  console.log(`Database connected!!!`);
+});
+
+app.get("/", (req, res) => {
+  const randomName = faker.name.findName();
+
+  const sql = `INSERT INTO people(name) values('${randomName}')`;
+  const select = `SELECT name from people`;
+
+  connection.query(sql);
+
+  connection.query(select, function (err, results, fields) {
+    res.send(response(results));
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Rodando na porta ${port}`);
+});
